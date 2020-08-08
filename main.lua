9164600c284cd356a277768ed4e30d654942fb9d
Kalen Chang <1614171+kalenchang@users.noreply.github.com>
2020-08-08 14:53:49 -0700

    added hoppiness
    constants need to be tweaked but overall can hop. can create problems if tempo is too high and the frog is not on the floor

--- main.lua
+++ main.lua
@@ -3,15 +3,62 @@ function love.load()
     windowY = 600
     love.window.setMode(windowX, windowY)
 
+    floor= 500
+
+    timer = 0
+    beat = 1
+    tempo = 60
+
+    gravy = 500
+
     frog = {
-        x = 300,
-        y = 300,
-        speed = 300
+        x = 100,
+        y = floor,
+        speed = 300, -- not used rn
+        angle = math.pi/3,
+        xvel = 0,
+        yvel = 0,
+    }
+
+    colors = {
+        darkGreen = convertColor("184d47"),
+        frogGreen = convertColor("96bb7c"),
+        lightGreen = convertColor("d6efc7"),
+        lightGrey = convertColor("aaaaaa"),
     }
 end
 
+function convertColor(hexcode)
+    --takes hexcode as a string, eg FFFFFF
+    local hexes = {
+        string.sub(hexcode, 1, 2),
+        string.sub(hexcode, 3, 4),
+        string.sub(hexcode, 5, 6),
+    }
+    local colorNumbers = {}
+    for i, hex in ipairs(hexes) do
+        table.insert(colorNumbers, tonumber(hex, 16)/255)
+    end
+
+    return colorNumbers
+end
+
 function love.update(dt)
-    moveToMouse(dt)
+    local frequency = tempo/60
+
+    timer = timer + dt * frequency
+    if timer > 1 then
+        timer = timer - 1
+        beat = beat + 1
+        if beat > 3 then
+            beat = beat - 3
+        end
+        frogHop(frog, beat)
+    end
+
+    frogMove(frog, dt)
+
+    --moveToMouse(dt)
 
 end
 
@@ -27,7 +74,29 @@ function love.draw()
         (math.floor(10*frog.x)/10)..', '..
         (math.floor(10*frog.y)/10)..')'
         , 10, 10)
+
+    love.graphics.print('velocity: ('..
+        (math.floor(10*frog.xvel)/10)..', '..
+        (math.floor(10*frog.yvel)/10)..')'
+        , 10, 30)
+
+    drawBeats(beat)
     
+end
+
+function drawBeats(b)
+    if b == 1 then
+        love.graphics.setColor(colors.darkGreen)
+    else
+        love.graphics.setColor(colors.lightGreen)
+    end
+    love.graphics.circle('fill', windowX - ((4-b) * 50), 30, 20)
+
+    --draw outines
+    love.graphics.setColor(0.6, 0.6, 0.6)
+    for i = 1, 3 do
+        love.graphics.circle('line', windowX - (i * 50), 30, 20)
+    end
 end
 
 function drawFrog(x, y)
@@ -53,4 +122,29 @@ function moveToMouse(dt)
     elseif frog.y > mouse.y then
         frog.y = frog.y - dt * frog.speed
     end
+end
+
+--TODO: add these functions to frog itself
+function frogHop(f, b)
+    local hopDistance = 100
+    if b == 1 then
+        hopDistance = 250
+    end
+    f.xvel = f.xvel + hopDistance * math.cos(f.angle)
+    f.yvel = f.yvel + hopDistance * math.sin(f.angle)
+
+end
+
+function frogMove(f, dt)
+    f.x = f.x + f.xvel * dt
+    f.y = f.y - f.yvel * dt
+    
+    if f.y > floor - 0.1 then
+        f.y = floor
+        f.xvel = 0
+        f.yvel = 0
+    else
+        f.yvel = f.yvel - gravy * dt
+    end
+
 end
